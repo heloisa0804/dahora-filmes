7be9a41aa87ddef0353c2c12df963c9c1cba8928
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-09-04 11:51:43 -0300

    Estiliza o botão inicial

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -18,10 +18,12 @@ export default function App() {
         <StatusBar style="auto" />
         <View style={estilos.viewLogo}>
           <Image source={require("./assets/dahora.png")} style={estilos.logo} />
-          <Text>Dá Hora Filmes</Text>
+          <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Button title="Buscar Filme" />
+          <Pressable style={estilos.botaoInicial}>
+            <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+          </Pressable>
           <Button title="Favoritos" />
         </View>
         <View style={estilos.viewRodape}>
@@ -50,12 +52,25 @@ const estilos = StyleSheet.create({
     width: 128,
     height: 128,
   },
+  tituloApp: {
+    fontSize: 32,
+    color: "#5451a6",
+    fontFamily: "Monoton",
+  },
   viewBotoes: {
     backgroundColor: "#ffcc80",
     flex: 2,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
+  },
+  botaoInicial: {
+    backgroundColor: "#5451ac",
+    padding: 16,
+    borderRadius: 8,
+  },
+  textoBotao: {
+    color: "white",
   },
   viewRodape: {
     backgroundColor: "#ef9a9a",
