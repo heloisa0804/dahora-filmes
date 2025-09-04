5311aa341fbc70407da1979918b268281f07121c
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-09-04 11:30:41 -0300

    Aplica fonte

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  if (!fonteCarregada) {
+    return <Text style={{ color: "red", fontSize: 22 }}>Carregando Fonte</Text>;
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={estilos.container}>
