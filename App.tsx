5277d253af9e67dd5a180db2a7f76a6bfda30898
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-09-18 08:22:56 -0300

    ...

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-// Importando a bliblioteca interna vector-icons
+// Importando a biblioteca interna vector-icons
 import { Ionicons } from "@expo/vector-icons";
+import Loading from "./src/components/Loading";
 
 export default function App() {
-  /* Usamos o useFonts para criar uma referência para a fonte que queremos utilizar no app. */
+  /* Usamos o useFonts para criar uma referência para 
+  a fonte que queremos utilizar no app. */
   const [fonteCarregada] = useFonts({
     Monoton: require("./assets/fonts/Monoton-Regular.ttf"),
   });
 
-  // Se a fonte ainda não for carregada
+  // Se a fonte ainda não foi carregada
   if (!fonteCarregada) {
-    return (
-      <Text style={{ color: "red", fontSize: 22 }}> Carregando fonte...</Text>
-    );
+    return <Loading />;
   }
 
   return (
@@ -28,65 +36,23 @@ export default function App() {
           <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Pressable
-            style={({ pressed }) => [
-              estilos.botaoInicial,
-              pressed && { backgroundColor: "black" },
-            ]}
-          >
-            <Ionicons
-              name="search"
-              size={24}
-              color="white"
-              style={{ marginRight: 8 }}
-            />
-            <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+          <Pressable style={estilos.botaoInicial}>
+            <Ionicons name="search" size={18} color="white" />
+            <Text style={estilos.textoBotao}> Buscar Filmes</Text>
           </Pressable>
-
-          <Pressable
-            style={({ pressed }) => [
-              estilos.botaoInicial,
-              pressed && { backgroundColor: "black" },
-            ]}
-          >
-            <Ionicons
-              name="star"
-              size={24}
-              color="yellow"
-              style={{ marginRight: 8 }}
-            />
-            <Text style={estilos.textoBotao}>Favoritos</Text>
+          <Pressable style={estilos.botaoInicial}>
+            <Ionicons name="star" size={18} color="gold" />
+            <Text style={estilos.textoBotao}> Favoritos</Text>
           </Pressable>
         </View>
         <View style={estilos.viewRodape}>
-          <Pressable
-            style={({ pressed }) => [
-              estilos.botaoRodape,
-              pressed && { backgroundColor: "black" },
-            ]}
-          >
-            <Ionicons
-              name="lock-closed"
-              size={18}
-              color="white"
-              style={{ marginRight: 7 }}
-            />
-            <Text style={estilos.textoRodape}>Privacidade</Text>
+          <Pressable style={estilos.botaoRodape}>
+            <Ionicons name="lock-closed" size={18} color="white" />
+            <Text style={estilos.textoBotao}> Privacidade</Text>
           </Pressable>
-
-          <Pressable
-            style={({ pressed }) => [
-              estilos.botaoRodape,
-              pressed && { backgroundColor: "black" },
-            ]}
-          >
-            <Ionicons
-              name="information-circle"
-              size={18}
-              color="white"
-              style={{ marginRight: 7 }}
-            />
-            <Text style={estilos.textoRodape}>Sobre</Text>
+          <Pressable style={estilos.botaoRodape}>
+            <Ionicons name="information-circle" size={18} color="white" />
+            <Text style={estilos.textoBotao}> Sobre</Text>
           </Pressable>
         </View>
       </SafeAreaView>
@@ -98,7 +64,7 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
-    paddingHorizontal: 12,
+    paddingHorizontal: 20,
     flex: 1,
     justifyContent: "center",
   },
@@ -108,54 +74,41 @@ const estilos = StyleSheet.create({
     alignItems: "center",
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 128,
+    height: 128,
   },
   tituloApp: {
     fontSize: 32,
     color: "#5451a6",
     fontFamily: "Monoton",
-    textAlign: "center",
-    marginBottom: 65,
   },
   viewBotoes: {
+    flex: 2,
     flexDirection: "row",
     justifyContent: "space-evenly",
-    alignItems: "center",
-    marginBottom: 240,
-    gap: 35,
+    alignItems: "flex-start",
   },
   botaoInicial: {
     backgroundColor: "#5451a6",
-    paddingVertical: 20,
-    paddingHorizontal: 16,
-    borderRadius: 10,
+    padding: 16,
+    borderRadius: 8,
     flexDirection: "row",
     alignItems: "center",
   },
   textoBotao: {
     color: "white",
-    fontSize: 16,
   },
   viewRodape: {
     backgroundColor: "#5451a6",
+    flex: 0.5,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    borderRadius: 10,
-    paddingHorizontal: 7,
-    paddingVertical: 12,
-    bottom: 15,
+    borderRadius: 8,
   },
   botaoRodape: {
-    paddingVertical: 14,
-    paddingHorizontal: 16,
-    borderRadius: 10,
+    padding: 16,
     flexDirection: "row",
     alignItems: "center",
-  },
-  textoRodape: {
-    color: "white",
-    fontSize: 14,
   },
 });
--- README.md
+++ README.md
@@ -1,20 +1,29 @@
-# Dá hora-filmes
+# Dá Horas Filmes
 
-Aplicativo desenvolvido ultilizando React Native com Expo
+Aplicativo desenvolvido utilizando React Native com Expo.
 
 ## 02_mais-estilos-logo-font-icones-pressable
 
-## 01_estrutura-e-estilizacao-basica01_estrutura-e-estilizacao-basica
+- Aplicação da imagem do logo usando o componente <Image>
+- Adição e configuração de fonte personalizada usando o `expo-font` e `useFonts`
+- Estilização do App.tsx usando recursos do `StyleSheet`
+- Substituição do `Button` por `Pressable` para criação de botões customizados
+- Uso de ícones através do `@expo-vector-icons`
+- Aplicação do `ActivityIndicator` para criação de um componente `Loading`
 
-- Remoção das imagens originais na pasta assests
-- Adição de imagens específicas para aplicativo
+---
+
+## 01_estrutura-e-estilizacao-basica
+
+- Remoção das imagens originais na pasta assets
+- Adição de imagens específicas para o aplicativo
 - Configuração dos detalhes do aplicativo em `app.json`
 - Instalação da dependência (lib) `react-native-safe-area-context`
 
 Use o comando: `npx expo install react-native-safe-area-context`
 
-_Ullizamos o `expo install` para garantir a instalação de dependências (ou libs) que sejam compátiveis com versao de SDK do Expo usada em seu projeto._
+_Utilizamos o `expo install` para garantir a instalação de dependências (ou libs) que sejam compatíveis com a versão de SDK do Expo usada em seu projeto._
 
-- Refatoramos a estrutura e estilização do `App.tsx` usando os componentes: `SafeAreaProvider`, `SafeAreaView`, `StatusBar`(do expo), `View`, `Text`, e `Button`
+- Refatoramos a estrutura e estilização do `App.tsx` usando os componentes: `SafeAreaProvider`, `SafeAreaView`, `StatusBar` (do expo), `View`, `Text` e `Button`
 
 - Uso do flex para determinar o tamanho das views.
