972050ce5653adf7617eee3fac38d1398b6185d0
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-09-18 11:14:33 -0300

    Adiciona link para conta

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 
 export default function index() {
   return (
@@ -20,20 +20,27 @@ export default function index() {
           <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Pressable style={estilos.botaoInicial}>
-            <Ionicons name="search" size={18} color="white" />
-            <Text style={estilos.textoBotao}> Buscar Filmes</Text>
-          </Pressable>
-          <Pressable style={estilos.botaoInicial}>
-            <Ionicons name="star" size={18} color="gold" />
-            <Text style={estilos.textoBotao}> Favoritos</Text>
-          </Pressable>
+          <Link href="/buscar" asChild>
+            <Pressable style={estilos.botaoInicial}>
+              <Ionicons name="search" size={18} color="white" />
+              <Text style={estilos.textoBotao}> Buscar Filmes</Text>
+            </Pressable>
+          </Link>
+
+          <Link href="/favoritos" asChild>
+            <Pressable style={estilos.botaoInicial}>
+              <Ionicons name="star" size={18} color="gold" />
+              <Text style={estilos.textoBotao}> Favoritos</Text>
+            </Pressable>
+          </Link>
         </View>
         <View style={estilos.viewRodape}>
-          <Pressable style={estilos.botaoRodape}>
-            <Ionicons name="lock-closed" size={18} color="white" />
-            <Text style={estilos.textoBotao}> Privacidade</Text>
-          </Pressable>
+          <Link href="/privacidade" asChild>
+            <Pressable style={estilos.botaoRodape}>
+              <Ionicons name="lock-closed" size={18} color="white" />
+              <Text style={estilos.textoBotao}> Privacidade</Text>
+            </Pressable>
+          </Link>
           <Pressable style={estilos.botaoRodape}>
             <Ionicons name="information-circle" size={18} color="white" />
             <Text style={estilos.textoBotao}> Sobre</Text>
