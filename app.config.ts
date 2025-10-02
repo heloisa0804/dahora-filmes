120a2c0b959207125d89c0b19b976fa474a43412
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-10-02 10:15:08 -0300

    Adiciona Id baseado diarebase

--- app.config.ts
+++ app.config.ts
@@ -29,6 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#000000",
     },
     edgeToEdgeEnabled: true,
+    package: ".com.heloisagbussi.dahorafilmes",
   },
   plugins: ["expo-font", "expo-router"],
   //Acessando a API_KEY através do dontenv e guardando na chave apiKey
